a807e1cbbfe6c4f4f52c904a27da1e1a12a1bd5e
ScWen7 <xxh783965@gmail.com>
2017-06-10 09:54:30 +0800

    添加Http  get或post 公共参数拦截器

--- app/src/main/java/com/xxh/mobilehelper/data/http/HttpParamInterceptor.java
+++ app/src/main/java/com/xxh/mobilehelper/data/http/HttpParamInterceptor.java
@@ -1,10 +1,20 @@
 package com.xxh.mobilehelper.data.http;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
 /**
  * Created by 解晓辉  on 2017/6/10 09:48 *
  * QQ  ：811733738
  * 作用:
  */
 
-public class HttpParamInterceptor {
+public class HttpParamInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
 }
