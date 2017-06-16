1897a9bb997ea01caf9e7c9a6078f2338ffe3eab
ScWen7 <ab811733738@163.com>
2017-06-16 09:44:58 +0800

    modify doc

--- app/src/main/java/com/xxh/mobilehelper/base/BaseMvpFragment.java
+++ app/src/main/java/com/xxh/mobilehelper/base/BaseMvpFragment.java
@@ -9,7 +9,7 @@ import com.xxh.mobilehelper.presenter.BasePresenter;
  * Created by 解晓辉  on 2017/6/10 14:52 *
  * QQ  ：811733738
  * 作用:   封装完成之后方法的调用顺序为:
- *   getLayoutId()   -> initView()  -> createPresenter() ->initData()
+ *   getLayoutId()   -> initView()  -> createPresenter() -> intiData()
  *
  *   生命周期  onDestoryView -> 中包含  presenter 的 detachView 和 presenter的置空
  *
