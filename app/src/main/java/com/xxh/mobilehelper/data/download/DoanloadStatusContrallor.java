adac5e815b95cb271be07823f30e6e0643b5c533
ScWen7 <ab811733738@163.com>
2017-06-27 15:29:00 +0800

    1、增加App 工具类和 File 工具类

---
+++ app/src/main/java/com/xxh/mobilehelper/data/download/DoanloadStatusContrallor.java
@@ -0,0 +1,37 @@
+package com.xxh.mobilehelper.data.download;
+
+import com.xxh.mobilehelper.bean.AppInfo;
+
+/**
+ * Created by 解晓辉 on 2017/6/27.
+ * 作用：
+ */
+
+public class DoanloadStatusContrallor {
+    public static void checkStatus(AppInfo appInfo) {
+
+    }
+
+
+    /**
+     * 判断App 是否安装
+     *
+     * @param packageName
+     * @return
+     */
+    public static boolean isInstalled(String packageName) {
+        return false;
+    }
+
+    /**
+     * 下载的文件是否存在
+     *
+     * @param packageName
+     * @return
+     */
+    public static boolean isFileExists(String packageName) {
+        return false;
+    }
+
+
+}
